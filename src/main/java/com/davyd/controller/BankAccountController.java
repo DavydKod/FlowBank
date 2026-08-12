bba78f5ae79772dd4feabd0f8dfba220256f356a
Davyd Maksimovich <dav.maks.dev@gmail.com>
2026-08-12 11:12:21 +0200

    feat: add user and account controllers

---
+++ src/main/java/com/davyd/controller/BankAccountController.java
@@ -0,0 +1,64 @@
+package com.davyd.controller;
+
+import com.davyd.dto.CreateBankAccountRequest;
+import com.davyd.models.BankAccount;
+import com.davyd.service.BankAccountService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class BankAccountController {
+
+    private final BankAccountService bankAccountService;
+
+    public BankAccountController(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BankAccount>> getAllAccounts() {
+        return ResponseEntity.ok(bankAccountService.getAllAccounts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BankAccount> getAccount(
+            @PathVariable @Positive long id
+    ) {
+        return ResponseEntity.ok(bankAccountService.getAccountById(id));
+    }
+
+    @GetMapping("/by-owner/{ownerId}")
+    public ResponseEntity<List<BankAccount>> getAccountsByOwner(
+            @PathVariable @Positive long ownerId
+    ) {
+        return ResponseEntity.ok(
+                bankAccountService.getAccountsByOwner(ownerId)
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<BankAccount> createAccount(
+            @RequestBody @Valid CreateBankAccountRequest request
+    ) {
+        BankAccount account = bankAccountService.createAccount(request.ownerId());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(account);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(
+            @PathVariable @Positive long id
+    ) {
+        bankAccountService.deleteAccount(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/davyd/dto/ChangeUserNameRequest.java
@@ -0,0 +1,11 @@
+package com.davyd.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChangeUserNameRequest(
+        @NotBlank
+        @Size(min = 3, max = 50)
+        String name
+) {
+}
