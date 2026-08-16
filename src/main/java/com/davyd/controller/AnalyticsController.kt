5b148cc65f25616bd83c223f25f81b8438030503
Davyd Maksimovich <dav.maks.dev@gmail.com>
2026-08-16 14:33:38 +0200

    feat: add analytics module

---
+++ src/main/java/com/davyd/controller/AnalyticsController.kt
@@ -0,0 +1,21 @@
+package com.davyd.controller
+
+import com.davyd.dto.AccountAnalytics
+import com.davyd.service.AnalyticsService
+import jakarta.validation.constraints.Positive
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/analytics")
+class AnalyticsController(private val analyticsService: AnalyticsService) {
+
+    @GetMapping("accounts/{id}")
+    fun getBankAccountAnalytics(@PathVariable("id") @Positive accountId: Long):
+            ResponseEntity<AccountAnalytics>{
+        return ResponseEntity.ok(analyticsService.getAccountAnalytics(accountId))
+    }
+}
---
+++ src/main/java/com/davyd/service/AnalyticsService.kt
@@ -0,0 +1,54 @@
+package com.davyd.service
+
+import com.davyd.repository.BankAccountRepository
+import com.davyd.repository.TransactionRepository
+import com.davyd.dto.AccountAnalytics
+import com.davyd.exception.BankAccountNotFoundException
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+@Service
+class AnalyticsService (
+    private val transactionRepository: TransactionRepository,
+    private val bankAccountRepository: BankAccountRepository
+){
+    fun getAccountAnalytics(accountId: Long): AccountAnalytics {
+        if (!bankAccountRepository.existsById(accountId)) {
+            throw BankAccountNotFoundException(accountId)
+        }
+
+        val totalSent: BigDecimal = transactionRepository.getTotalSent(accountId)
+
+        val totalReceived: BigDecimal = transactionRepository.getTotalReceived(accountId)
+
+        val sentTransactionCount: Long = transactionRepository.getSentTransactionCount(accountId)
+
+        val receivedTransactionCount: Long = transactionRepository.getReceivedTransactionCount(accountId)
+
+        val transactionCount: Long = sentTransactionCount + receivedTransactionCount
+
+        val averageTransactionSent: BigDecimal = if (sentTransactionCount == 0L){
+            BigDecimal.ZERO} else {
+                totalSent.divide(BigDecimal.valueOf(sentTransactionCount),
+                    2, RoundingMode.HALF_UP)
+        }
+
+        val averageTransactionReceived: BigDecimal = if (receivedTransactionCount == 0L){
+            BigDecimal.ZERO} else {
+            totalReceived.divide(BigDecimal(receivedTransactionCount),
+                2,
+                RoundingMode.HALF_UP)
+        }
+
+        val largestSentTransaction: BigDecimal? = transactionRepository.getLargestSentTransaction(accountId)
+
+        val largestReceivedTransaction: BigDecimal? = transactionRepository.getLargestReceivedTransaction(accountId)
+
+        return AccountAnalytics(accountId = accountId, totalSent = totalSent, totalReceived = totalReceived,
+            transactionCount = transactionCount, sentTransactionCount = sentTransactionCount,
+            receivedTransactionCount = receivedTransactionCount,
+            averageTransactionSent = averageTransactionSent, averageTransactionReceived = averageTransactionReceived,
+            largestSentTransaction = largestSentTransaction, largestReceivedTransaction = largestReceivedTransaction)
+    }
+}
