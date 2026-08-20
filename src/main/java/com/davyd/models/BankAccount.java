17359d8d2f8dd9c13ea049687db15afb4e3d0227
Davyd Maksimovich <dav.maks.dev@gmail.com>
2026-08-20 20:00:30 +0200

    feat: add optimistic locking to bank accounts

--- src/main/java/com/davyd/models/BankAccount.java
+++ src/main/java/com/davyd/models/BankAccount.java
@@ -25,6 +25,10 @@ public class BankAccount {
     @Column(nullable = false)
     private AccountStatus status;
 
+    @Version
+    @Column(nullable = false)
+    private Long version;
+
 
     protected BankAccount() {
     }
