0285007a3cbe8d1c5c9e892df319e617d695f6d5
Davyd Maksimovich <dav.maks.dev@gmail.com>
2026-08-23 17:40:26 +0200

    test: transferLimitService tests

--- src/test/java/com/davyd/service/TransferLimitServiceTest.java
+++ src/test/java/com/davyd/service/TransferLimitServiceTest.java
@@ -49,4 +49,68 @@ class TransferLimitServiceTest {
                 )
         );
     }
+
+    @Test
+    void shouldAllowTransferWhenDailyLimitNotExceeded() {
+        User user = new User("Joel", "joel@gmail.com");
+
+        BankAccount account = new BankAccount(user);
+
+        BigDecimal alreadyTransferred = new BigDecimal("800.00");
+        BigDecimal newTransferAmount = new BigDecimal("150.00");
+
+        when(
+                transactionRepository.getTotalSentSince(
+                        eq(account.getId()),
+                        any(LocalDateTime.class)
+                )
+        ).thenReturn(alreadyTransferred);
+
+        transferLimitService.validateDailyTransferLimit(account, newTransferAmount);
+    }
+
+    @Test
+    void shouldAllowTransferWhenDailyLimitNotExceededOnEdgeCase() {
+        User user = new User("Joel", "joel@gmail.com");
+
+        BankAccount account = new BankAccount(user);
+
+        BigDecimal alreadyTransferred = new BigDecimal("850.00");
+        BigDecimal newTransferAmount = new BigDecimal("150.00");
+
+        when(
+                transactionRepository.getTotalSentSince(
+                        eq(account.getId()),
+                        any(LocalDateTime.class)
+                )
+        ).thenReturn(alreadyTransferred);
+
+        transferLimitService.validateDailyTransferLimit(account, newTransferAmount);
+    }
+
+    @Test
+    void shouldThrowWhenBigDecimalNull(){
+        User user = new User("Joel", "joel@gmail.com");
+
+        BankAccount account = new BankAccount(user);
+
+        assertThrows(IllegalArgumentException.class, () ->
+                transferLimitService.validateDailyTransferLimit(account, null));
+    }
+
+    @Test
+    void shouldThrowWhenBankAccountNull(){
+        assertThrows(IllegalArgumentException.class, () ->
+                transferLimitService.validateDailyTransferLimit(null, BigDecimal.valueOf(10)));
+    }
+
+    @Test
+    void shouldThrowWhenBigDecimalExceededCharacterLimit(){
+        User user = new User("Joel", "joel@gmail.com");
+
+        BankAccount account = new BankAccount(user);
+
+        assertThrows(IllegalArgumentException.class, () ->
+                transferLimitService.validateDailyTransferLimit(account, BigDecimal.valueOf(100000000000000000L)));
+    }
 }
